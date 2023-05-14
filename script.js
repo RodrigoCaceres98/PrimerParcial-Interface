c2c8041f3ca3fb23c629fab328ff0fe9833498fa
Rodro <66178007+RodrigoCaceres98@users.noreply.github.com>
2023-05-14 00:40:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+const RedireccionarARegistro = () => {
+    window.location.href = "register.html"
+}
+
+const RedireccionarAMenu = () => {
+    window.location.href = "menu.html"
+}
+
+// const CambiarDeModo = () => {
+//     CambiarDeModoOscuroAClaroODeOscuroAClaro();
+//     cambiarColorDeHeader();
+// }
+
+
+// const CambiarDeModoOscuroAClaroODeOscuroAClaro = () => {
+
+//     const tieneBlanco = document.querySelector('body').classList.contains('claro')
+//     if (tieneBlanco) {
+//         document.querySelector('body').classList.remove('claro')
+//         document.querySelector('body').classList.add('oscuro')
+//         document.querySelector('p').classList.add('letraClara')
+//     }
+//     else {
+//         document.querySelector('body').classList.remove('oscuro')
+//         document.querySelector('body').classList.add('claro')
+//         document.querySelector('p').classList.add('letraOscura')
+//     }
+// }
+
