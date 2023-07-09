41be8b2741bd0319c77bb792e4cd09d059812295
RodrigoCaceres98 <rodrigoramoncaceres@gmail.com>
2023-07-09 20:38:46 -0300

    faltan formulario y redireccionamiento

--- script.js
+++ script.js
@@ -5,8 +5,25 @@ const RedireccionarARegistro = () => {
     window.location.href = "register.html"
 }
 
-const RedireccionarAMenu = () => {
-    window.location.href = "menu.html"
+
+const DireccionarABeneficios = () => {
+    console.log("entro a la funcion beneficios")
+    const section = document.getElementById("seccionBeneficios"); // Obtener la referencia a la sección
+    section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
+    // scrollToSection("seccionBeneficios")
+}
+
+const DireccionarAMenu = () => {
+    console.log("entro a la funcion menu")
+    const section = document.getElementById("menuHamburguesas"); // Obtener la referencia a la sección
+    section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
+
+    // scrollToSection("menuHamburguesas")
+}
+
+function scrollToSection(sección) {
+    const section = document.getElementById(sección); // Obtener la referencia a la sección
+    section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
 }
 
 // const CambiarDeModo = () => {
