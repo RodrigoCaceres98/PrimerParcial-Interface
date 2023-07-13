1c11cd2eb75af49116cabded2458c31ba4e69ddd
RodrigoCaceres98 <rodrigoramoncaceres@gmail.com>
2023-07-12 23:44:12 -0300

    lading

--- script.js
+++ script.js
@@ -7,43 +7,20 @@ const RedireccionarARegistro = () => {
 
 
 const DireccionarABeneficios = () => {
-    console.log("entro a la funcion beneficios")
-    const section = document.getElementById("seccionBeneficios"); // Obtener la referencia a la sección
-    section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
-    // scrollToSection("seccionBeneficios")
+    scrollToSection("seccionBeneficios")
+}
+
+const DireccionarAFooter = () => {
+    scrollToSection("footer")
 }
 
 const DireccionarAMenu = () => {
-    console.log("entro a la funcion menu")
-    const section = document.getElementById("menuHamburguesas"); // Obtener la referencia a la sección
-    section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
-
-    // scrollToSection("menuHamburguesas")
+    scrollToSection("menuHamburguesas")
 }
 
-function scrollToSection(sección) {
-    const section = document.getElementById(sección); // Obtener la referencia a la sección
+const scrollToSection = (sección) => {
+    const section = document.getElementById(sección);
     section.scrollIntoView({ behavior: "smooth" }); // Realizar un desplazamiento suave hacia la sección
 }
 
-// const CambiarDeModo = () => {
-//     CambiarDeModoOscuroAClaroODeOscuroAClaro();
-//     cambiarColorDeHeader();
-// }
-
-
-// const CambiarDeModoOscuroAClaroODeOscuroAClaro = () => {
-
-//     const tieneBlanco = document.querySelector('body').classList.contains('claro')
-//     if (tieneBlanco) {
-//         document.querySelector('body').classList.remove('claro')
-//         document.querySelector('body').classList.add('oscuro')
-//         document.querySelector('p').classList.add('letraClara')
-//     }
-//     else {
-//         document.querySelector('body').classList.remove('oscuro')
-//         document.querySelector('body').classList.add('claro')
-//         document.querySelector('p').classList.add('letraOscura')
-//     }
-// }
 
